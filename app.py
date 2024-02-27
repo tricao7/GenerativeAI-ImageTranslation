76b6343ecb9c03f004580a67764348d06638a322
Tri Cao <tcao5555@gmail.com>
2024-02-26 20:05:07 -0800

    Update app.py - include TTS

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
-from image_to_text import pretrained, caption_text
+from image_captioning import *
 
 UPLOAD_FOLDER = 'static/images'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -11,6 +11,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Initialize the pre-trained model
 processor, model = pretrained()
+tts_tokenizer, tts_model = pretrained_tts()
 
 
 def allowed_file(filename):
@@ -41,7 +42,10 @@ def home_page():
 def uploaded_file(filename):
     '''This is the uploaded file page'''
     file_path = os.path.join('static', 'images', filename)
+    # Generate Caption
     caption = caption_text(processor, model, file_path)
+    # Add Audio to Caption
+    audio_path = tts(tts_tokenizer, tts_model, caption)
     
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -56,7 +60,8 @@ def uploaded_file(filename):
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file', filename=filename))
 
-    return render_template('response.html', image_file=file_path, caption=caption)
+    return render_template('response.html', image_file=file_path, caption=caption,
+                           audio_file=audio_path)
 
 
 @app.route('/test')
@@ -64,20 +69,36 @@ def testing_page():
     '''This is the testing page'''
     return render_template('tester.html')
 
-
-if __name__ == "__main__":
-    port = os.environ.get('PORT', 5000)
+def delete_files():
+    '''
+    This function deletes all the files that were uploaded by
+    the user.
+    '''
     base_images = os.path.join('static', 'images')
     iamges = ['header_background.jpg', 'sample1.jpg',
               'sample2.jpg', 'sample3.jpeg', 'sample4.jpg']
-    # delete images taht are not in the base images
+    base_audio = os.path.join('static', 'audio')
+    audio = ['sample1.wav', 'sample2.wav',
+             'sample3.wav', 'sample4.wav']
+    # delete images that are not in the base images
     # Note: This is to prevent user images from being saved
     for file in os.listdir(base_images):
         if file not in iamges:
             os.remove(os.path.join(base_images, file))
+    # delete audio that are not in the base audio
+    # Note: This is to prevent user audio from being saved
+    for file in os.listdir(base_audio):
+        if file not in audio:
+            os.remove(os.path.join(base_audio, file))
+
+
+if __name__ == "__main__":
+    port = os.environ.get('PORT', 8080)
     # Run the app
     app.run(debug=True, host='0.0.0.0', port=port)
-    # if the app is closed, the model will be deleted
+    # if the app is closed, the model, processor
+    # and the files will be deleted
+    delete_files()
     del processor
     del model
 
